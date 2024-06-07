bcde11f1f3c7a8a01d5f656e4ea78f99807f0a4a
Harry Field <rz20505@it102011.wks.bris.ac.uk>
2024-06-07 11:16:06 +0100

    restructuring

--- methods/label-llama-3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-torchrun --nproc_per_node 1 label-llama.py \
-    --ckpt_dir /home/rz20505/Documents/ask-jgi/Meta-Llama-3-8B-Instruct/ \
-    --tokenizer_path /home/rz20505/Documents/ask-jgi/Meta-Llama-3-8B-Instruct/tokenizer.model \
-    --max_seq_len 1024 --max_batch_size 2
--- methods/label-test-llama.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-torchrun --nproc_per_node 1 label-test-llama.py \
-    --ckpt_dir /home/rz20505/Documents/ask-jgi/Meta-Llama-3-8B-Instruct/ \
-    --tokenizer_path /home/rz20505/Documents/ask-jgi/Meta-Llama-3-8B-Instruct/tokenizer.model \
-    --max_seq_len 1024 --max_batch_size 2
